931852e26b6b4e987d58e4701101c6c5530a2432
princeps091-binf <princeps091@gmail.com>
2022-02-03 12:00:18 +0100

    Decipher latent cell-line grouping using community detection on correlation graph

--- code/cage_peak_explore.R
+++ code/cage_peak_explore.R
@@ -2,10 +2,44 @@ library(tidyverse)
 library(vroom)
 library(igraph)
 library(seriation)
+library(viridisLite)
 
 cage_tbl<-vroom("~/Documents/multires_bhicect/data/epi_data/hg19.cage_peak_phase1and2combined_tpm_ann.osc.txt",comment = '#',col_select = contains(c("Annotation","cell%20line")))
 cell_line<-colnames(cage_tbl)
 sample_id<-unlist(lapply(strsplit(cell_line,split='\\.'),'[',3))
+colnames(cage_tbl)<-c("peak.ID",sample_id[-1])
+cage_tbl<-cage_tbl[-c(1,2),]
+#Produce correlation matrix across cell-lines
+cell_cor_mat<-cor(cage_tbl[,-1],method = "spearman")
+save(cell_cor_mat,file = './data/CAGE_cormat.Rda')
+
+base::load('./data/CAGE_cormat.Rda')
+d<-as.dist(1-cell_cor_mat)
+o<-seriate(d,method="TSP")
+image(cell_cor_mat[get_order(o),get_order(o)],col=viridis(100))
+
+cell_cor_mat[cell_cor_mat<median(as.numeric(cell_cor_mat))]<-0
+
+g_cell<-graph_from_adjacency_matrix(cell_cor_mat,weighted = T,diag = F,mode = "undirected")
+#two samples isolated once we set correlation threshold
+main_g_v<-names(which(components(g_cell)$membership==1))
+
+main_sub_g<-induced_subgraph(g_cell,main_g_v)
+louvain_sample_cluster<-cluster_louvain(main_sub_g)
+table(cluster_louvain(main_sub_g)$membership)
+grep(paste(V(main_sub_g)$name[which(cluster_louvain(main_sub_g)$membership==3)],collapse = "|"),cell_line,value=T)
+##Build the corresponding clustering matrix
+sample_comm<-unique(louvain_sample_cluster$membership)
+comm_edge_tbl<-do.call(bind_rows,lapply(sample_comm,function(i){
+  tmp_v<-V(main_sub_g)$name[which(cluster_louvain(main_sub_g)$membership==i)]
+  expand_grid(ego=tmp_v,alter=tmp_v) %>% mutate(x=i)
+}))
+tmp_mat<-matrix(0,nrow = nrow(cell_cor_mat),ncol=ncol(cell_cor_mat),dimnames = dimnames(cell_cor_mat))
+tmp_mat[as.matrix(comm_edge_tbl[,1:2])]<-comm_edge_tbl$x
+image(tmp_mat[get_order(o),get_order(o)],col=plasma(4))
+
+
+#Pivot to more efficient long-format
 long_transform_tbl<-do.call(bind_rows,lapply(2:ncol(cage_tbl),function(i){
   
   cage_tbl %>% dplyr::select(1,i) %>%
@@ -19,6 +53,8 @@ long_transform_tbl<-do.call(bind_rows,lapply(2:ncol(cage_tbl),function(i){
 
 save(long_transform_tbl,file = './data/long_form_cage_tbl.Rda')
 base::load('./data/long_form_cage_tbl.Rda')
+
+
 
 sample_summary_tbl<-long_transform_tbl %>% 
   dplyr::rename(peak.ID=`00Annotation`) %>% 
